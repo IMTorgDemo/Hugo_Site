175c011890bd7a0ec0cb45bcb3b984f587c63908
IMTorg <information@mgmt-tech.org>
2022-08-07 23:13:05 -0400

    rebuilding site Sun Aug  7 23:12:53 EDT 2022

--- deploy.sh
+++ deploy.sh
@@ -7,20 +7,20 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 hugo -b "https://imtorgdemo.github.io/"    # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 cd ..
-cp -r Hugo_IMTorgDemo/IMTorgDemo.github.io/.git Hugo_Site/public/
-sudo rm -r Hugo_IMTorgDemo/IMTorgDemo.github.io/
-cp -r Hugo_Site/public/ Hugo_IMTorgDemo/IMTorgDemo.github.io/
+cp -r Hugo_Out-Site/IMTorgDemo.github.io/.git Hugo_Site_Repo/public/
+sudo rm -r Hugo_Out-Site/IMTorgDemo.github.io/
+cp -r Hugo_Site_Repo/public/ Hugo_Out-Site/IMTorgDemo.github.io/
 
 # Push and commit github.io
 msg="rebuilding site `date`"
  
-cd Hugo_IMTorgDemo/IMTorgDemo.github.io/
+cd Hugo_Out-Site/IMTorgDemo.github.io/
 git add -A
 git commit -m "$msg"
 git push origin master
 
 # Push and commit Hugo_Site
-cd ../../Hugo_Site
+cd ../../Hugo_Site_Repo
 sudo rm -r public/.git
 git add -A
 git commit -m "$msg"
